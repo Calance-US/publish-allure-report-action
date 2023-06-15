f199489d7166349678356931b6d48c2c8b7920ed
Ritik232 <rmittal@calance.com>
2023-06-15 18:03:30 +0530

    error handeling of codebase

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const core = require('@actions/core')
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 const fetch = require('node-fetch');
 
 const apiUrl = "https://qa-reports.calance.work/allure-api/allure-docker-service";
 
 const loginCredentials = {
-    username: process.env.username,
-    password: process.env.password
+    username:  core.getInput('username'),
+    password:  core.getInput('password')
 };
 const project = core.getInput('project_name');
 
@@ -19,130 +19,119 @@ async function checkApiStatus() {
             body: JSON.stringify(loginCredentials),
             headers: { 'Content-Type': 'application/json' }
         });
+        const message = await response.json();
+        if (response.status !== 200) {
+            throw new Error (`Login failed! ${JSON.stringify(message, null, 2)}`)
+        }
+        const cookies = response.headers.get('set-cookie')
+        const lines = cookies.split(";");
+        const filteredLines = lines.filter(line => !line.includes("Secure") && !line.includes("HttpOnly"));
 
-        const statusCode = response.status;
-        if (statusCode === 200) {
-            core.info('Login API status is 200. Success!');
-            const cookies = response.headers.get('set-cookie')
+        const cookieArray = filteredLines.map(value => {
+            let newVal = value.replace(' Path=/,', '')
+            newVal = value.replace(' Path=/', '')
 
-            const lines = cookies.split(";");
-            const filteredLines = lines.filter(line => !line.includes("Secure") && !line.includes("HttpOnly"));
+            return newVal;
+        });
+        let finalCookie = cookieArray.join("")
+        finalCookie = finalCookie.replaceAll(",", ";")
 
-            const cookieArray = filteredLines.map(value => {
-                let newVal = value.replace(' Path=/,', '')
-                newVal = value.replace(' Path=/', '')
+        //get projects
+        const resp = await fetch(`${apiUrl}/projects`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json', 'cookie': finalCookie },
+        });
+        const data = await resp.json()
 
-                return newVal;
+        if (resp.status !== 200) {
+            throw new Error (`Failed to get projects. ${JSON.stringify(data, null, 2)}`)
+        }
+
+        const projects = data.data.projects
+
+        keys = Object.keys(projects)
+
+        const exists = keys.includes(project);
+        const match = finalCookie.match(/csrf_access_token=([^;]+)/);
+        const csrfAccessToken = match[1];
+
+        if (exists === false) {
+            //create project
+            const res = await fetch(`${apiUrl}/projects`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    id: project
+                }),
+                headers: { 'Content-Type': 'application/json', 'cookie': finalCookie, 'X-CSRF-TOKEN': csrfAccessToken }
             });
-            let finalCookie = cookieArray.join("")
-            finalCookie = finalCookie.replaceAll(",", ";")
 
-            //get projects
-
-            const resp = await fetch(`${apiUrl}/projects`, {
-                method: 'GET',
-                headers: { 'Content-Type': 'application/json', 'cookie': finalCookie },
-            });
-            const data = await resp.json()
-
-            if (resp.status !== 200) {
-                core.setFailed(`Something went wrong in getting projects. Status Code: ${resp.status}`)
+            const newProject = await res.json()
+            if (res.status !== 201) {
+                throw new Error (`Failed to create project. ${JSON.stringify(newProject, null, 2)}`)
             }
+        }
 
-            const projects = data.data.projects
-
-            keys = Object.keys(projects)
-
-            const exists = keys.includes(project);
-            const match = finalCookie.match(/csrf_access_token=([^;]+)/);
-            const csrfAccessToken = match[1];
-
-            if (exists === false) {
-                //create project
-                const res = await fetch(`${apiUrl}/projects`, {
-                    method: 'POST',
-                    body: JSON.stringify({
-                        id: project
-                    }),
-                    headers: { 'Content-Type': 'application/json', 'cookie': finalCookie, 'X-CSRF-TOKEN': csrfAccessToken }
-                });
-
-                if (res.status !== 201) {
-                    core.setFailed(`Something went wrong in creating project. Status Code: ${res.status}`)
-                }
-
-                // const result = await response.json()
-            } else {
-                console.log("Project already exists")
+        const folderPath = core.getInput('results_path');
+        async function readdir() {
+            const file = await fs.readdir(folderPath)
+            return file
+        }
+        const files = await readdir()
+        console.log(files)
+        let result = [];
+        files.forEach((file) => {
+            const filePath = path.join(folderPath, file);
+            const extension = filePath.split('.').pop().toLowerCase();
+            if (extension !== 'json' && extension !== 'xml') {
+                return
+            } else{
+                result.push(filePath)
             }
-
-            const folderPath = core.getInput('results_path');
-
-            fs.readdir(folderPath, (err, files) => {
-                if (err) {
-                    console.error('Error reading folder:', err);
-                    return;
-                }
-                const results = []
-                files.forEach((file) => {
-                    const filePath = path.join(folderPath, file);
-                    const extension = filePath.split('.').pop().toLowerCase();
-                    if (extension !== 'json' && extension !== 'xml') {
-                        return
-                    }
-
-                    let encodedData = {}
-                    fs.readFile(filePath, 'utf8', async (err, data) => {
-                        if (err) {
-                            console.error('Error reading file:', err);
-                            return;
-                        }
-
-                        const buffer = Buffer.from(data, 'utf8');
-
-                        const base64Data = buffer.toString('base64');
-
-                        results.push({
-                            content_base64: base64Data,
-                            file_name: file,
-                        });
-
-                        if (results.length === files.length) {
-                            const output = {
-                                results: results,
-                            };
-
-                            const filesData = JSON.stringify(output)
-                            const res = await fetch(`${apiUrl}/send-results?project_id=${project}&force_project_creation=false`, {
-                                method: 'POST',
-                                body: filesData,
-                                headers: { 'Content-Type': 'application/json', 'cookie': finalCookie, 'X-CSRF-TOKEN': csrfAccessToken }
-                            });
-
-                            if (res.status !== 200) {
-                                core.setFailed(`Something went wrong in sending result. Status Code: ${res.status}`)
-                            }
-
-                            const report = await fetch(`${apiUrl}/generate-report?project_id=${project}`, {
-                                method: 'GET',
-                                headers: { 'Content-Type': '', 'cookie': "" },
-                            });
-                            if (report.status !== 200) {
-                                core.setFailed(`Something went wrong in generating report. Status Code: ${report.status}`)
-                                console.log("Failed")
-                            }
-                            const generatedReprot = await report.json()
-                            console.log(generatedReprot)
-                        }
-                    });
+        })
+        let encodedData = []
+        for (let i = 0; i < result.length; i++) {
+            const filePath = result[i];
+            let results = [];
+            async function readfile() {
+                const fileData = await fs.readFile(filePath)
+                const buffer = Buffer.from(fileData, 'utf8');
+                const base64Data = buffer.toString('base64');
+                results.push({
+                    content_base64: base64Data,
+                    file_name: files[i],
                 });
-            });
+                return results
+            }
+            const data = await readfile()
+            encodedData = [...encodedData, ...data];
+        }
+        object = {
+            results: encodedData
+        }
 
-        } else {
-            core.setFailed(`Login API status is ${statusCode}.Error occurred.`);
+        const filesData = JSON.stringify(object)
+        const res = await fetch(`${apiUrl}/send-results?project_id=${project}&force_project_creation=false`, {
+            method: 'POST',
+            body: filesData,
+            headers: { 'Content-Type': 'application/json', 'cookie': finalCookie, 'X-CSRF-TOKEN': csrfAccessToken }
+        });
+        resultsApiData = await res.json()
+        if (res.status !== 200) {
+            throw new Error (`Failed to create project. ${JSON.stringify(resultsApiData, null, 2)}`)
+        }
+        const report = await fetch(`${apiUrl}/generate-report?project_id=${project}`, {
+            method: 'GET',
+            headers: { 'Content-Type': '', 'cookie': "" },
+        });
+
+        const generatedReprot = await report.json()
+        if (report.status !== 200) {
+            throw new Error (`Failed to create project. ${JSON.stringify(generatedReprot, null, 2)}`)
+        } else{
+            console.log(generatedReprot)
         }
     } catch (error) {
-        console.error('Error occurred while hitting the API:', error);
+        core.setFailed('Error occurred while hitting the API:', error.message);
     }
 }
 
